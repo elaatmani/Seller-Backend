160fcaf17e4db506adae669bafa1b8a0408a2d2e
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-02-07 22:56:01 +0100

    in update

--- app/Http/Controllers/Api/Admin/ProductController.php
+++ app/Http/Controllers/Api/Admin/ProductController.php
@@ -174,8 +174,8 @@ class ProductController extends Controller
                 $productValidator = Validator::make($request->all(),
                 [
                 'name' => 'required',
-                'buying_price' => 'required',
-                'quantity' => 'required'
+                'buying_price' => 'required|integer',
+                'quantity' => 'required|integer'
                 ]);
 
                 if($productValidator->fails()){
