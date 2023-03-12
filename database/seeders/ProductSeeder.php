c6cc6620cabd23729d4459a38dbd9997ae6866c7
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-03-12 09:50:30 +0100

    seeder product agentes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -14,14 +15,39 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::create([
-            'ref' => 'backpack',
-            'name' => 'Backpack',
-            'buying_price' => 5,
-            'selling_price' => 15,
-            'description' => 'Product Backpack',
-            'status' => 1
-        ]);
 
+
+        $products = [
+            [
+                'ref' => 'backpack',
+                'name' => 'Backpack',
+                'buying_price' => 5,
+                'selling_price' => 15,
+                'description' => 'Product Backpack',
+                'status' => 1
+            ],
+            [
+                'ref' => 'iphone',
+                'name' => 'Iphone',
+                'buying_price' => 5,
+                'selling_price' => 15,
+                'description' => 'Product Iphone',
+                'status' => 1 
+            ]
+        ];
+
+        foreach ($products as $product) {
+            $productModel = Product::create($product);
+            DB::table('product_agentes')->insert([
+                [
+                    'agente_id' => 2,
+                    'product_id' => $productModel->id,
+                ],
+                [
+                    'agente_id' => 2,
+                    'product_id' => $productModel->id,
+                ],
+            ]);
+        }
     }
 }
