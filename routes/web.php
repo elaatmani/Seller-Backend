95a1960acdc45cbc02c036c2102447c51757c17d
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-09-01 16:04:49 +0100

    codequad

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::get('/road', function() {
 // });
 
 Route::get('storage/productImages/{filename}', function ($filename) {
-    $path = "/home/u594122495/domains/vldo.shop/public_html/seller/api/storage/app/public/productImages/{$filename}";
+    $path = "/home/u594122495/domains/codesquad.net/public_html/seller/api/storage/app/public/productImages/{$filename}";
     if (file_exists($path)) {
         return response()->file($path);
     } else {
