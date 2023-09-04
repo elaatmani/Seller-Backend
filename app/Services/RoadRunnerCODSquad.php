07c5b288723e68e7ccae8c8293625d02a1e9ec13
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-09-05 00:24:42 +0100

    fixed

--- app/Services/RoadRunnerCODSquad.php
+++ app/Services/RoadRunnerCODSquad.php
@@ -157,7 +157,8 @@ class RoadRunnerCODSquad
 
     public static function getPrice($order) {
         if (!$order) return 0;
-        $total = array_reduce($order['items']->values()->toArray(), function($sum, $item) {
+        unset($order->items);
+        $total = array_reduce($order->items->values()->toArray(), function($sum, $item) {
             return $sum + (!$item['price'] ? 0 : $item['price']);
         }, 0);
             return round(floatval(!$order['price'] ? 0 : $order['price']) + floatval($total), 2);
